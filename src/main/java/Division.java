c3fa56c06c3d3cfa855094ba7db69d120b83c1b0
Vennela <vennelaie202@gmail.com>
2024-03-19 15:15:42 +0530

    Division of 2 byte num

---
+++ src/main/java/Division.java
@@ -0,0 +1,16 @@
+package com.qualizeal.javaapp.mathematics;
+	public class Division{
+		public void div(){	
+         	 byte a = 40,b = 20;
+         	 System.out.println("result" + a / b);
+		
+
+
+          
+
+
+
+}
+
+
+}
---
+++ src/main/java/Program.java
@@ -0,0 +1,14 @@
+package com.qualizeal.javaapp.mainprogram;
+
+import com.qualizeal.javaapp.mathematics.Division;
+  
+    public class Program {
+	public static void main(String[] args){	
+               Division division = new Division();
+                division.div();
+	
+
+
+}
+}
+ 
