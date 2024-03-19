3802a12bb7cb62cff3cf8cb7051e3e94a11e5fd7
Vennela <vennelaie202@gmail.com>
2024-03-19 15:28:52 +0530

    multiplication

---
+++ src/main/java/Multiplication.java
@@ -0,0 +1,10 @@
+package com.qualizeal.javaapp.mathematics;
+	public class Multiplication{
+		public void mul(){	
+         	 byte a = 40,b = 20;
+         	 System.out.println("result" + a * b);
+}
+}
+		
+
+
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,11 +1,11 @@
 package com.qualizeal.javaapp.mainprogram;
 
-import com.qualizeal.javaapp.mathematics.Division;
+import com.qualizeal.javaapp.mathematics.Multiplication;
   
     public class Program {
 	public static void main(String[] args){	
-               Division division = new Division();
-                division.div();
+               Multiplication multiplication = new Multiplication();
+               multiplication.mul();
 	
 
 
